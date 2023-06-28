5fb39f566d9865e6a162eb8835428e16011366b2
DESKTOP-BHDQH8H\vohungvi <vohungvi27@gmail.com>
2023-06-28 19:53:39 +0700

    + add console example

---
+++ ConsoleCs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGMT;
+
+namespace ConsoleCs
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            if(args.Length == 1)
+            {
+                CitizenIDReader reader = new CitizenIDReader();
+                string result = reader.Read(args[0]);
+                //Console.WriteLine(result);
+
+                return int.Parse(result);
+            }
+            return 0;
+        }
+    }
+}
